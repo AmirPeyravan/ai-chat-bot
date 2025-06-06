67b014128921bdf2b9a34de245846b4eaf1a0012
amirpeyravan <141130613+AmirPeyravan@users.noreply.github.com>
2025-06-07 00:11:28 +0330

    Update ChatControllr.php

--- app/Http/Controllers/ChatControllr.php
+++ app/Http/Controllers/ChatControllr.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+
 
 
 class ChatController extends Controller
